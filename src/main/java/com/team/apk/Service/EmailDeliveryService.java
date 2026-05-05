a5d8c58ff392f0b70bab2bd9f5d988452176cfd0
medjinetoussaint541-ctrl <medjinetoussaint541@gmail.com>
2026-05-04 20:14:52 -0400

    Update EmailDeliveryService.java

--- src/main/java/com/team/apk/Service/EmailDeliveryService.java
+++ src/main/java/com/team/apk/Service/EmailDeliveryService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Async;
 
 //Service responsable de l'envoi des emails.
 //Ici, il sert surtout à envoyer le code de vérification.
